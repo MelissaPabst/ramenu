fc4d6ee28bb6972b220931b5de11641fb95db983
Melissa Pabst <melissapabst@gmail.com>
2018-07-26 00:20:49 -0500

    created fragments, started work on User class

--- src/main/java/org/launchcode/ramenu/controllers/LandingController.java
+++ src/main/java/org/launchcode/ramenu/controllers/LandingController.java
@@ -1,16 +1,18 @@
 package org.launchcode.ramenu.controllers;
 
 
+import org.launchcode.ramenu.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("ramenu")
 public class LandingController {
 
     @RequestMapping(value = "")
-
     public String index(Model model) {
 
         model.addAttribute("title", "Welcome to Ramenu!");
