700faba3f68c44e2d336b5b3544200068beb7daa
Melissa Pabst <melissapabst@gmail.com>
2018-07-25 23:39:00 -0500

    changed controller name

--- src/main/java/org/launchcode/ramenu/controllers/LandingController.java
+++ src/main/java/org/launchcode/ramenu/controllers/LandingController.java
@@ -4,11 +4,10 @@ package org.launchcode.ramenu.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("ramenu")
-public class HelloController {
+public class LandingController {
 
     @RequestMapping(value = "")
 
