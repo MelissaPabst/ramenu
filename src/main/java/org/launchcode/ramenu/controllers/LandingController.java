5b221fad35b519a2455fa3c3dd43c6f16ba76a4d
Melissa Pabst <melissapabst@gmail.com>
2018-08-01 23:44:18 -0500

    started working with data; seeded database

--- src/main/java/org/launchcode/ramenu/controllers/LandingController.java
+++ src/main/java/org/launchcode/ramenu/controllers/LandingController.java
@@ -1,12 +1,10 @@
 package org.launchcode.ramenu.controllers;
 
 
-import org.launchcode.ramenu.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("ramenu")
@@ -17,6 +15,24 @@ public class LandingController {
 
         model.addAttribute("title", "Welcome to Ramenu!");
         return "index";
+    }
+
+    @RequestMapping(value = "login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute("title", "Log In | Ramenu");
+        return "user/login";
+    }
+
+    @RequestMapping(value = "signup")
+    public String displaySignupForm(Model model) {
+        model.addAttribute("title", "Sign UP | Ramenu");
+        return "user/signup";
+    }
+
+    @RequestMapping(value = "order")
+    public String displayOrderForm(Model model){
+        model.addAttribute("title", "Order | Ramenu");
+        return "order/broth";
     }
 
 }
---
+++ src/main/java/org/launchcode/ramenu/models/data/ingredients.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Aug 02, 2018 at 06:24 AM
+-- Server version: 5.6.38
+-- PHP Version: 7.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `ingredients` (
+  `id` int(11) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(30) NOT NULL,
+  `price` (3) decimal(19,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+INSERT INTO `ingredients` (`id`, `type`, `name`, `description`, `price`) VALUES
+
+
+(1, 'broth', 'Pork', 'Made from the best pork meat', 2),
+(2, 'broth', 'Chicken', 'Made from the meanest chickens', 2),
+(3, 'broth', 'Veggie', 'Made from basic veggies.', 2),
+
+(4, 'noodle', 'Flat', 'Thin, skinny cut.', 3),
+(5, 'noodle', 'Fat', 'Wide, thick cut.', 3),
+(6, 'noodle', 'Gluten-Free', 'Rice noodles', 3),
+
+(7, 'topping', 'Mushrooms', 'Wood Ear, Local', 2),
+(8, 'topping', 'Scallions', 'Green Onions', 1),
+(9, 'topping', 'Burnt Ends', 'Pork goodness', 3),
+(10, 'topping', 'Bacon', 'Hipster Staple', 1),
+(11, 'topping', 'Chicken', 'Roasted', 3),
+(12, 'topping', 'Egg', 'Boiled', 1),
+(13, 'topping', 'Tofu', 'Fermented Soybeans', 2),
+(14, 'topping', 'Bean Sprouts', 'Um.', 1),
+(15, 'topping', 'Corn', 'Grows on a cob. Roasted', 1),
+(16, 'topping', 'Kimchi', 'Fermented stuff', 1),
+(17, 'topping', 'Steamed Vegetables', 'zucchini, onions, broccoli, ca', 2),
+(18, 'topping', 'Gyoza', 'Pork Dumplings', 3),
+(19, 'topping', 'Roasted Pork', 'Pork not in a dumpling.', 3),
+(20, 'topping', 'Spinach', 'Locally-grown', 1),
+(21, 'topping', 'Bamboo Shoots', 'Not from around here', 1);
+
