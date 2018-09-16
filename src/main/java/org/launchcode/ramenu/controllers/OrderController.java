e07093849bb75f4baad12b6b1f559bbde73a5aff
Melissa Pabst <melissapabst@gmail.com>
2018-09-16 12:31:46 -0500

    cleans up code for master branch

--- src/main/java/org/launchcode/ramenu/controllers/OrderController.java
+++ src/main/java/org/launchcode/ramenu/controllers/OrderController.java
@@ -1,17 +1,12 @@
 package org.launchcode.ramenu.controllers;
 
 import org.launchcode.ramenu.models.RamenOrder;
-import org.launchcode.ramenu.models.Ingredient;
 import org.launchcode.ramenu.models.data.IngredientDao;
 import org.launchcode.ramenu.models.data.RamenOrderDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Controller
 @RequestMapping("ramenu")
@@ -49,17 +44,10 @@ public class OrderController {
         model.addAttribute(new RamenOrder());
         model.addAttribute("ingredients", ingredientDao.findAll());
 
-////        creates new ramen order and prints ID to verify order is created
-//        RamenOrder newRamenOrder = new RamenOrder();
-//        ramenOrderDao.save(newRamenOrder);
-//        int newRamenOrderId = newRamenOrder.getId();
-//        System.out.println("ID="+ newRamenOrderId);
-////        return "order/ingredients/" + newRamenOrderId;
         return "order/ingredients";
 
     }
-//  ingredients/{newRamenOrderId}
-//  @PathVariable int newRamenOrderId,
+
     @RequestMapping(value = "order", method = RequestMethod.POST)
     public String processOrderForm(@ModelAttribute RamenOrder newRamenOrder,
                                    @RequestParam int brothId,
@@ -69,44 +57,11 @@ public class OrderController {
                                    Model model)
     {
 
-////        create list to hold ingredientIds
-//        List<Integer> ingredientIds = new ArrayList<>();
-//
-////        loop through all ids and get ingredient id and put into list
-//        ingredientIds.add(brothId);
-//        ingredientIds.add(noodleId);
-//        for (int toppingId : toppingIds){
-//            ingredientIds.add(toppingId);
-//        }
-//
-////      take list and put into order
-//        int newRamenOrderId = newRamenOrder.getId();
-//        System.out.println(newRamenOrder.getId());
-//
-//        for (int ingredientId : ingredientIds) {
-//            Ingredient ingredient = ingredientDao.findOne(ingredientId);
-//            ramenOrderDao.findOne(newRamenOrderId);
-//            newRamenOrder.addItem(ingredient);
-//        }
-
-//        RamenOrder newRamenOrder = ramenOrderDao.findOne(newRamenOrderId);
         newRamenOrder.setTotal(total);
 
 //      add order to dao and save
         ramenOrderDao.save(newRamenOrder);
 
-
-
-//         Print tests
-        System.out.println("ORDER ID="+ newRamenOrder.getId());
-        System.out.println("BROTH ID=" + brothId);
-        System.out.println("NOODLE ID=" + noodleId);
-        System.out.println("TOPPING IDS=" + toppingIds[0]);
-//        System.out.println("INGREDIENT IDS=" + ingredientIds);
-        System.out.println("TOTAL=" + total);
-
-
-//        return "order/ordersummary/" + newRamenOrder.getId();
         return "order/ordersummary";
     }
 
