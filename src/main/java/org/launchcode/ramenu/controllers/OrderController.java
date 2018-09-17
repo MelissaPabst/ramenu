9b031b439fd9852d2a77ba97bdc9a6989df03710
Melissa Pabst <melissapabst@gmail.com>
2018-09-17 13:02:39 -0500

    saves list of ingredients MTM

--- src/main/java/org/launchcode/ramenu/controllers/OrderController.java
+++ src/main/java/org/launchcode/ramenu/controllers/OrderController.java
@@ -1,5 +1,6 @@
 package org.launchcode.ramenu.controllers;
 
+import org.launchcode.ramenu.models.Ingredient;
 import org.launchcode.ramenu.models.RamenOrder;
 import org.launchcode.ramenu.models.data.IngredientDao;
 import org.launchcode.ramenu.models.data.RamenOrderDao;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -58,6 +61,30 @@ public class OrderController {
                                    @RequestParam int[] toppingIds,
                                    @RequestParam float total,
                                    Model model) {
+
+//        create list to hold ingredientIds
+        List<Integer> ingredientIds = new ArrayList<>();
+
+//        loop through all ids and get ingredient id and put into list
+        ingredientIds.add(brothId);
+        ingredientIds.add(noodleId);
+        for (int toppingId : toppingIds) {
+            ingredientIds.add(toppingId);
+        }
+
+//        take list and put into order
+        int newRamenOrderId = newRamenOrder.getId();
+
+        List<Ingredient> ingredients = new ArrayList<>();
+
+        for (int ingredientId : ingredientIds) {
+            Ingredient ingredient = ingredientDao.findOne(ingredientId);
+            ingredients.add(ingredient);
+        }
+        ramenOrderDao.findOne(newRamenOrderId);
+
+//          findAllById(Iterable<ID> ids)
+        newRamenOrder.setIngredients(ingredients);
 
         newRamenOrder.setTotal(total);
 
