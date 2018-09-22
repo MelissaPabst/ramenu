f0a4857b1bf5b636797e56ec975d061c0362eb2e
Melissa Pabst <melissapabst@gmail.com>
2018-09-21 20:08:38 -0500

    updates controller

--- src/main/java/org/launchcode/ramenu/controllers/OrderController.java
+++ src/main/java/org/launchcode/ramenu/controllers/OrderController.java
@@ -1,12 +1,18 @@
 package org.launchcode.ramenu.controllers;
 
+import org.launchcode.ramenu.models.Ingredient;
 import org.launchcode.ramenu.models.RamenOrder;
 import org.launchcode.ramenu.models.data.IngredientDao;
 import org.launchcode.ramenu.models.data.RamenOrderDao;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("ramenu")
@@ -54,15 +60,54 @@ public class OrderController {
                                    @RequestParam int noodleId,
                                    @RequestParam int[] toppingIds,
                                    @RequestParam float total,
-                                   Model model)
-    {
+                                   Model model) {
+
+//        create list to hold ingredientIds
+        List<Integer> ingredientIds = new ArrayList<>();
+
+//        loop through all ids and get ingredient id and put into list
+        ingredientIds.add(brothId);
+        ingredientIds.add(noodleId);
+        for (int toppingId : toppingIds) {
+            ingredientIds.add(toppingId);
+        }
+
+//        take list and put into order
+        int newRamenOrderId = newRamenOrder.getId();
+
+//        create list to hold ingredients
+        List<Ingredient> ingredients = new ArrayList<>();
+
+//        loop through ingredientIds and add to ingredients
+        for (int ingredientId : ingredientIds) {
+            Ingredient ingredient = ingredientDao.findOne(ingredientId);
+            ingredients.add(ingredient);
+        }
+
+        newRamenOrder.setIngredients(ingredients);
 
         newRamenOrder.setTotal(total);
 
-//      add order to dao and save
         ramenOrderDao.save(newRamenOrder);
 
         return "order/ordersummary";
     }
 
+
+    @RequestMapping(value = "order/{newRamenOrderId}", method = RequestMethod.GET)
+    public String viewOrder(Model model, @PathVariable int newRamenOrderId) {
+
+
+        RamenOrder newRamenOrder = ramenOrderDao.findOne(newRamenOrderId);
+
+        model.addAttribute("title", "Review Order Number: " + newRamenOrder.getId());
+
+        model.addAttribute("newRamenOrderId", newRamenOrder.getId());
+        model.addAttribute("ingredients", newRamenOrder.getIngredients());
+        model.addAttribute("total", newRamenOrder.getTotal());
+
+        return "order/ordersummary" + newRamenOrderId;
+    }
+
 }
+
